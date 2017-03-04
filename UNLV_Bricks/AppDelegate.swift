88570aa6e821352fb90c8bb2d6f6ac229efdada7
Carlos Lemus <carlosslemus@yahoo.com>
2017-03-04 14:30:06 -0800

    Add sounds, blocks, particles

--- UNLV_Bricks/AppDelegate.swift
+++ UNLV_Bricks/AppDelegate.swift
@@ -7,15 +7,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let musicPlay = NSURL(fileURLWithPath: Bundle.main.path(forResource: "game_music", ofType: "mp3")!)
+        do{
+            let audioPlayer = try AVAudioPlayer(contentsOf:musicPlay as URL)
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+            audioPlayer.numberOfLoops = -1
+            
+        }catch {
+            print("Error getting the audio file")
+        }
+
         return true
     }
 
--- UNLV_Bricks/GameScene.swift
+++ UNLV_Bricks/GameScene.swift
@@ -16,6 +16,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     
     private var isFingerOnPaddle = false
     
+    private var lives = 3
     //Create category bit maskes (identify the category a body belongs to)
     let BallCategory   : UInt32 = 0x1 << 0
     let BottomCategory : UInt32 = 0x1 << 1
@@ -23,29 +24,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     let PaddleCategory : UInt32 = 0x1 << 3
     let BorderCategory : UInt32 = 0x1 << 4
     
+    let brickHitSound = SKAction.playSoundFileNamed("break", waitForCompletion: false)
+    let paddleHitSound  = SKAction.playSoundFileNamed("paddle_hit", waitForCompletion: false)
+    let bottomHitSound  = SKAction.playSoundFileNamed("lose_live", waitForCompletion: false)
+    
+    var backgroundMusic: SKAudioNode!
     override func didMove(to view: SKView) {
-        
-       /* // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-        */
-        
        
         let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame) //create an edge-based body, does not have mass or volume and is unnafected by forces
         
@@ -55,19 +39,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         
         
         //Define blocks
-        let numberOfBlocks = 8
-        let blockWidth = SKSpriteNode(imageNamed: "block").size.width
-        let totalBlcksWidth = blockWidth * CGFloat(numberOfBlocks)
+        let numberOfBlocks = 71
+       // let blockWidth = SKSpriteNode(imageNamed: "block").size.width
+        let blockWidth = CGFloat(100)
         
         //calclulate offset between blocks
-        let xOffset = (frame.width - totalBlcksWidth)/2
-        
+        var xOffset = CGFloat(25)
+        var yOffset = CGFloat(1158.98)
+        var counter = 0
         //create blocks
-        for i in 0..<numberOfBlocks {
+        for i in 1..<numberOfBlocks {
             let block = SKSpriteNode(imageNamed: "block.png")
-            block.position = CGPoint(x: xOffset + CGFloat(CGFloat(i) + 0.5) * blockWidth,
-                                     y: frame.height * 0.8)
-            
+            block.position = CGPoint(x: xOffset + CGFloat(CGFloat(counter) + 0.5) * blockWidth,
+                                     y: yOffset)
+            counter += 1
             block.physicsBody = SKPhysicsBody(rectangleOf: block.frame.size)
             block.physicsBody!.allowsRotation = false
             block.physicsBody!.friction = 0.0
@@ -76,7 +61,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
             block.name = "block"
             block.physicsBody!.categoryBitMask = BlockCategory
             block.zPosition = 2
+            block.size.width = 100
+            block.size.height = 20
             addChild(block)
+            
+            
+            if(i % 7 == 0){
+                yOffset -= block.size.height //create a new row
+                counter = 0
+            }
         }
         
         
@@ -89,7 +82,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         let ball = childNode(withName: "ball") as! SKSpriteNode
         let paddle = childNode(withName: "paddle") as! SKSpriteNode
         
-        ball.physicsBody!.applyImpulse(CGVector(dx: 30.0, dy: -30.0))
+        ball.physicsBody!.applyImpulse(CGVector(dx: 35.0, dy: -40.0))
         
         let bottomRect = CGRect(x: frame.origin.x, y: frame.origin.y, width: frame.size.width, height: 1)
         let bottom = SKNode()
@@ -105,8 +98,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         paddle.physicsBody!.categoryBitMask = PaddleCategory
         borderBody.categoryBitMask = BorderCategory
         
-        ball.physicsBody!.contactTestBitMask = BottomCategory
-        paddle.physicsBody!.contactTestBitMask = BottomCategory
+        ball.physicsBody!.contactTestBitMask = BottomCategory | BlockCategory | PaddleCategory
+        
+        //Create a particle trail for the ball
+        let trailNode = SKNode()
+        trailNode.zPosition = 1
+        addChild(trailNode)
+        let trail = SKEmitterNode(fileNamed: "BallTrail")!
+        trail.targetNode = trailNode
+        ball.addChild(trail)
+        
+        
+        if let musicURL = Bundle.main.url(forResource: "game_music", withExtension: "mp3") {
+            backgroundMusic = SKAudioNode(url: musicURL)
+            addChild(backgroundMusic)
+        }
     }
     
     
@@ -127,39 +133,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         
         //Check if the Ball has made contact with the bottom of the screen
         if firstBody.categoryBitMask == BallCategory && secondBody.categoryBitMask == BottomCategory {
-            print("Hit bottom. First contact has been made.")
+            print("Hit bottom. ")
+            let display = childNode(withName: "lbl_winlose") as! SKLabelNode
+            if(lives > 0){
+                display.text = String(lives)
+                lives -= 1
+            }else{
+                display.text = "You Lose"
+            }
+            run(bottomHitSound)
         }
-        //Check if the Ball has made contact with the paddle
+        //Check if the Ball has made contact with the blcok
+        if firstBody.categoryBitMask == BallCategory && secondBody.categoryBitMask == BlockCategory {
+            print("Hit block.")
+            breakBlock(node: secondBody.node!)
+            run(brickHitSound)
+        }
+        //Check if the Ball has made contact with the blcok
         if firstBody.categoryBitMask == BallCategory && secondBody.categoryBitMask == PaddleCategory {
-            print("Hit paddle. First contact has been made.")
-        }
-        //Check if the Ball has made contact with the paddle
-        if secondBody.categoryBitMask == BallCategory && firstBody.categoryBitMask == PaddleCategory {
-            print("Hit paddle. First contact has been made.")
-        }
-    }
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        /*if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }*/
-        
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+            print("Hit paddle.")
+            run(paddleHitSound)
         }
     }
     
@@ -178,9 +171,29 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
             if body.node!.name == "paddle"{
                 print("Began touch on paddle")
                 isFingerOnPaddle = true;
+                
+                //if paddle is touched give the ball just a little movement in case the ball is stuck going side to side or up and down
+                let ball = childNode(withName: "ball") as! SKSpriteNode
+                
+                //Adjust ball if stuck
+                if(ball.position.y <= 20 ){
+                    
+                    ball.physicsBody!.applyImpulse(CGVector(dx: 1, dy: 40))
+                }
+                if(ball.position.y >= 1180 ){
+                    
+                    ball.physicsBody!.applyImpulse(CGVector(dx: 1, dy: -40))
+                }
+                if(ball.position.x >= 720){
+                    
+                    ball.physicsBody!.applyImpulse(CGVector(dx: -40, dy: 1))
+                }
+                if(ball.position.x <= 20){
+                    
+                    ball.physicsBody!.applyImpulse(CGVector(dx: 40, dy: 1))
+                }
             }
         }
-        
         
     }
     
@@ -189,12 +202,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         
         //NOTE: change the anchor point for the scene to 0,0 for the paddleX Max and Min function
         
+        //update position of paddle on finger
+        let touch = touches.first
+        let touchLocation = touch!.location(in: self)
+        let previousLocation = touch!.previousLocation(in: self)
+        
         //check wether player is touching paddle
         if isFingerOnPaddle {
-            //update position of paddle on finger
-            let touch = touches.first
-            let touchLocation = touch!.location(in: self)
-            let previousLocation = touch!.previousLocation(in: self)
             
             //get the paddle SKSpriteNode
             let paddle = childNode(withName: "paddle") as! SKSpriteNode
@@ -209,7 +223,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
             //Set the paddle position
             paddle.position = CGPoint(x: paddleX, y: paddle.position.y)
             
+        }
+        if(touchLocation.x <= 10 && touchLocation.y <= 10){
+            let display = childNode(withName: "lbl_winlose") as! SKLabelNode
             
+            display.text = "You Win"
+            lives = 3
         }
     }
     
@@ -218,12 +237,36 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         isFingerOnPaddle = false
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        if(isGameWon() ){
+            let display = childNode(withName: "lbl_winlose") as! SKLabelNode
+            
+            display.text = "You Win, Welcome to UNLV"
+        }
+    }
+    
+    func isGameWon()  -> Bool{
+        //Check all the bricks in the game and return true if it is zero
+        var numberOfBricks = 0
+        self.enumerateChildNodes(withName: "block") {
+            node, stop in
+            numberOfBricks = numberOfBricks + 1
+        }
+        return numberOfBricks == 0
+    }
+    
+    func breakBlock(node: SKNode){
+        //Create an emiter node from the file
+        let particles = SKEmitterNode(fileNamed: "BrokenPlatform")!
+        particles.position = node.position
+        particles.zPosition = 3
+        addChild(particles) //add particles to the position of the broken blcok
+        particles.run(SKAction.sequence([SKAction.wait(forDuration: 1.0), SKAction.removeFromParent()]))
+        node.removeFromParent()//remove block
+        
     }
 }
